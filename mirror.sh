3bdbb13fce7ad976a64e36eafa895c90254d2380
Tiziano Müller <tiziano.mueller@hpe.com>
2025-10-03 15:56:02 +0200

    mirror.sh: list locations for easier copy&paste

--- mirror.sh
+++ mirror.sh
@@ -2,13 +2,22 @@
 
 set -euo pipefail
 
+list_baseline_locations() {
+    echo "Found the following baseline locations:"
+    find /assets -name masterdependency.xml -print0 -or -name prerequisite_bp.xml -print0 | xargs -r0 dirname
+}
+
+
 if [ "$#" -ne 1 ] ; then
     echo "Usage: $0 <SDR-REPO>"
     echo ""
     echo "Mirrors an HPE Linux SDR Repo to /assets/<SDR-REPO> using lftp, for consumption by SUM."
     echo "Example:"
-    echo "  $0 spp-gen10/2025.07.00.00"
+    echo "  $0 spp-gen10/2025.09.00.00"
+    echo ""
+    list_baseline_locations
     exit 1
 fi
 
 exec lftp -c mirror -x 'vmw/$' --parallel=5 "https://downloads.linux.hpe.com/SDR/repo/$1" "/assets/$1"
+list_baseline_locations
